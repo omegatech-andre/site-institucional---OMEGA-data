0dd47c99c710684f16bb225a3258e6185d157aeb
omegatech-andre <omega.ti_ind@outlook.com>
2023-12-18 17:15:01 -0300

    adicionando rota / com link para rota /dataProducts

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use('/public', express.static('public'));
 const catalogModel = require('./models/products')
 
 
-app.get('/dataProducts', async(req, res) => {
+app.get('/', async (req, res) => {
+  return res.status(200).send('<a href="/dataProducts">/dataProducts</a>')
+})
+
+app.get('/dataProducts', async (req, res) => {
   return res.status(200).json({
     catalog: await catalogModel.find({})
   })
@@ -31,6 +35,6 @@ app.get('/dataProducts', async(req, res) => {
 
 
 //GETWAY
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('servidor no ar')
 })
