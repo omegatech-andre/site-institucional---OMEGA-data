47f4991229d1ff25795c14470b4bbb75072e3c2b
omegatech-andre <omega.ti_ind@outlook.com>
2023-12-22 17:02:06 -0300

    add new layout on route /

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
+const favicon = require('serve-favicon')
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+// Favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 //DATA CONNECTION
 const conn = require('./data/conn')
@@ -16,7 +22,13 @@ const catalogModel = require('./models/products')
 
 
 app.get('/', async (req, res) => {
-  return res.status(200).send('<a href="/dataProducts">/dataProducts</a>')
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    res.send(data)
+  })
 })
 
 app.get('/dataProducts', async (req, res) => {
